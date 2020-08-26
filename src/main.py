3900ab08f204d6548c173ddc5f33cc95c6e4918a
azychen <antonzychen+github@gmail.com>
2020-08-26 10:57:41 -0600

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""
+Investment Helper (2020)
+
+A web application which suggests personalized stock investments, 
+using a machine learning model trained on historical market activity.
+
+Repository link: https://github.com/azychen/investment-helper
+
+Contributors: 
+
+    Anton Chen (https://github.com/azychen)
+    Annie Liu (https://github.com/annieliu10)
+
+This file is the entry point to the program.
+"""
